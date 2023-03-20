f74ef53e48d1b1254a61bd683139e699bce6c287
Erik Henrique <erikhrodrigues@gmail.com>
2023-03-20 11:02:59 -0300

    Adicionando cards

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./Form.css"
+import {AiOutlineDelete} from 'react-icons/ai'
+import {GrEdit} from 'react-icons/gr'
 
 const Form = () => {
 
@@ -7,7 +9,7 @@ const Form = () => {
     const [time, setTime] = useState("");
     const [priority, setPriority] = useState("");
     const [data, setData] = useState([])
-    
+    const [urgency, setUrgency] = useState([])
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -17,30 +19,48 @@ const Form = () => {
             time,
             priority
         }
-        setData([...data, {todo}]);
+        createUrgency(todo);
+        setData([...data, todo]);
     };
+
+    const createUrgency = (todo) => {
+        todo.priority === "Urgency" && setUrgency([...urgency, todo])
+        console.log(urgency)
+    }
+
+    const handleDelete = (id) => {
+        console.log(id)
+        setData(data.filter(todo => (todo.id !== id)))
+    }
+
     return (
         <div>
             <form className="form" onSubmit={handleSubmit}>
                 <label name="todo">What your To-do?</label>
-                <input type="text" name="todo" onChange={(e) => setTitle(e.target.value)}></input>
+                <input type="text" name="todo" onChange={(e) => setTitle(e.target.value)} required></input>
                 <label name="time">How many time do you need?(hours)</label>
-                <input type="text" name="time" onChange={(e) => setTime(e.target.value)}></input>
+                <input type="text" name="time" onChange={(e) => setTime(e.target.value)} required></input>
                 <label name="priority">What your priority in this to-do?</label>
-                <select onChange={(e) => setPriority(e.target.value)}>
+                <select onChange={(e) => setPriority(e.target.value)} required>
+                    <option></option>
                     <option>Urgency</option>
                     <option>for more Late</option>
                     <option>Only for not forget</option>
                 </select>
                 <button type="submit">Submit</button>
             </form>
+            <h2>To-do's</h2>
             <div className="todo-list">
-                <h2>To-do's</h2>
                 {data.length === 0 && <p>Não há tarefas...</p>}
                 {data.map((t) => (
-                    <div>
-                        <p key={t.id}>Title: {t.title}</p>
-                        <p key={t.id+1}>Time:{t.time}</p>
+                    <div key={t.id} className={t.priority}>
+                        <p>Title: {t.title}</p>
+                        <p>Time:{t.time}</p>
+                        <p>Priority:{t.priority}</p>
+                        <div className="icons">
+                            <span className="delete" onClick={() => handleDelete(t.id)}><AiOutlineDelete/></span>
+                            <span className="edit"><GrEdit/></span>
+                        </div>
                     </div>
                 ))}
             </div>
---
+++ src/components/groups/Groups.js
@@ -0,0 +1,12 @@
+import "./Groups.css"
+import Form from "../form/Form"
+
+const Groups = () => {
+    return (
+        <div>
+            <Form/>
+        </div>
+    )
+}
+
+export default Groups
